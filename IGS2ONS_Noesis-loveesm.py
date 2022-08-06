083f268f8d743a8c106796eaa5c7ba87f80fe6d3
Prince-of-sea <taichi.okazaki2@gmail.com>
2022-08-06 22:24:49 +0900

    初版リリース

---
+++ IGS2ONS_Noesis-loveesm.py
@@ -0,0 +1,250 @@
+#!/usr/bin/env python3
+from PIL import Image, ImageEnhance
+import subprocess
+import shutil
+import glob
+import sys
+import os
+import re
+
+same_hierarchy = (os.path.dirname(sys.argv[0]))#同一階層のパスを変数へ代入
+EXE_IGS = os.path.join(same_hierarchy,'igscriptD.exe')
+DEFAULT_TXT = os.path.join(same_hierarchy,'default.txt')
+
+#same_hierarchy = os.path.join(same_hierarchy,'Gungnir_loveesm_EXT')#debug
+DIR_SCR = os.path.join(same_hierarchy,'script')
+DIR_SCR_DEC = os.path.join(same_hierarchy,'script_dec')
+
+DIR_BG = os.path.join(same_hierarchy,'bgimage')
+DIR_FG = os.path.join(same_hierarchy,'fgimage')
+DIR_SYS = os.path.join(same_hierarchy,'system')
+DIR_IMG = os.path.join(same_hierarchy,'image')
+
+jmp_cnt = 0
+jmp_list = []
+
+
+def img_resize():
+	l = []
+	l += glob.glob(os.path.join(DIR_BG, '*.png'))
+	l += glob.glob(os.path.join(DIR_FG, '*.png'))
+	l += glob.glob(os.path.join(DIR_SYS, '*.png'))
+
+	os.makedirs(DIR_IMG, exist_ok=True)
+
+	for p in l:
+		p_name = os.path.basename(p)
+		p_r = os.path.join(DIR_IMG, p_name)
+
+		im = Image.open(p)
+		w = int(im.width * 600 / 768)
+		h = int(im.height * 600 / 768)
+
+		im_r = im.resize((w, h), Image.Resampling.LANCZOS)
+
+		#画像個別処理
+		if str(p_name).lower() == 'window_base.png':
+			im_r.putalpha(192)
+
+		elif str(p_name).lower() == 'load_base.png':
+			enhancer = ImageEnhance.Brightness(im_r)
+			im_r = enhancer.enhance(0.4)
+
+		elif str(p_name).lower() == 'config_base.png':
+			enhancer = ImageEnhance.Brightness(im_r)
+			im_r = enhancer.enhance(0.2)
+
+		elif str(p_name).lower() == 'view_cg.png':
+			enhancer = ImageEnhance.Brightness(im_r)
+			im_r = enhancer.enhance(0.2)
+
+		im_r.save(p_r)
+
+
+def text_dec():
+	os.makedirs(DIR_SCR_DEC, exist_ok=True)
+
+	for p in glob.glob(os.path.join(DIR_SCR, '*.s')):
+		n = (os.path.splitext(os.path.basename(p))[0])
+		subprocess.run([EXE_IGS, '-p', p, n+'.txt'], shell=True, cwd=DIR_SCR)
+
+	for p in glob.glob(os.path.join(DIR_SCR, '*.txt')):
+		shutil.move(p, DIR_SCR_DEC)
+
+
+def text_cnv():
+	global jmp_cnt
+	with open(DEFAULT_TXT) as f:
+		txt = f.read()
+
+	for p in glob.glob(os.path.join(DIR_SCR_DEC, '*.txt')):
+		with open(p, encoding='cp932', errors='ignore') as f:
+
+			name = os.path.splitext(os.path.basename(p))[0]
+			txt += '\nerrmsg:reset\n;--------------- '+ name +' ---------------\n*SCR_'+ name +'\n'
+			OPJ_flag = False
+
+			for line in f:
+				goto_line = re.search(r'[0-9\s]+\s([0-9A-z-_]+?)\.s', line)
+				txt_line = re.search(r'0400\s[A-z0-9]{4}\s(＃)?(.+?)\n', line)
+				bg_line = re.search(r'(040F|0410)\s[0-9A-z]{4}\s(.+?)\.bmp', line)
+				tatiset_line = re.search(r'0412\s[A-z0-9]{4}\s(.+?)\n', line)
+				tatiload_line = re.search(r'0814 0000 01F4 0000', line)
+				tatireset_line = re.search(r'0811 0000 0000 0000', line)
+				wait_line = re.search(r'080E 0000 0BB8 0000', line)
+				wait2_line = re.match(r'080E 0000 05DC 0000', line)
+				black_line = re.search(r'0816 0001 0000 0000', line)
+				bgmstop_line = re.search(r'0824 0000 03E8 0000', line)
+				se_line = re.match(r'([A-z0-9_-]+?)\n', line)
+				spmsg_line = re.match(r'049C\s0901\s(.+?)\.png', line)
+				spmsg_end_line = re.match(r'0475 0001 043F', line)
+				OPTIONJUMP_line = re.match(r'!OPTIONJUMP!', line)
+				#txt_line = re.search(r'', line)
+
+				if re.search('^\n', line):#空行
+					pass#そのまま放置
+
+				elif goto_line:
+					line = 'goto *SCR_' + goto_line[1] + '\n'
+
+				elif txt_line:
+
+					if txt_line[1]:
+						line = 'mov $10,"' + txt_line[2] + '"\n'
+
+					else:
+						msg = re.sub(r'\<(.+?)\<(.+?)\>', r'(\1/\2)', txt_line[2])
+						line = msg + '\\\n'
+
+				elif bg_line:
+					line = 'vsp 9,0:vsp 8,0:print 1:bg "image\\' + bg_line[2] + '.png",10\n'
+
+				elif tatiset_line:
+					line = 'tati_set "' + tatiset_line[1] + '","' + tatiset_line[1][:3] + '"\n'
+
+				elif tatiload_line:
+					line = 'tati_load\n'
+
+				elif tatireset_line:
+					line = 'tati_reset\n'
+
+				elif wait_line:
+					line = 'wait 2000\n'
+
+				elif wait2_line:
+					line = 'wait 2000\n'
+
+				elif black_line:
+					line = 'vsp 9,0:vsp 8,0:print 1:bg black,10\n'
+
+				elif bgmstop_line:
+					line = 'bgmstop\n'
+
+				elif se_line:
+					line = 'def_SEset "' + se_line[1] + '"\n'
+
+				elif spmsg_line:
+					line = 'spmsg "' + spmsg_line[1] + '"\n'
+
+				elif spmsg_end_line:
+					line = 'spmsg_end\n'
+
+				elif OPTIONJUMP_line:
+					OPJ_flag = True
+
+					jmp_cnt += 1
+					line = r';JMP_CNT' + str(jmp_cnt) + '_\n'
+				
+				elif OPJ_flag:
+					OPJ_flag = False
+					jmp_list.append(line[:-1])
+					line = r';' + line#エラー防止の為コメントアウト
+
+				else:
+					line = '\n'
+					#line = r';' + line#エラー防止の為コメントアウト
+
+				txt += line
+
+			txt += '\n;SCR_'+ name +'_END'
+
+	#-----ガ バ ガ バ 修 正-----
+	#OP
+	txt = txt.replace(r'goto *SCR_0020', 'mpegplay "video\\op.mpg",1\ngoto *SCR_0020')
+	#選択肢1
+	txt = txt.replace(r';JMP_CNT1_', r'select "'+jmp_list[0]+r'",*S1A,"'+jmp_list[1]+r'",*S1B')
+	txt = txt.replace(r'がっちりと腰', r'*S1A'+'\nがっちりと腰')
+	txt = txt.replace(r'慌てて引', r'goto *S1END'+'\n*S1B\n慌てて引')
+	txt = txt.replace('微かだった。\\', '微かだった。\\\n*S1END\n')
+	#選択肢2
+	txt = txt.replace(r';JMP_CNT3_', r'select "'+jmp_list[2]+r'",*S2A,"'+jmp_list[3]+r'",*S2B'+'\n*S2A')
+	txt = txt.replace(r'引き抜いた―', 'goto *S2END\n*S2B\n引き抜いた―')
+	txt = txt.replace(r'「……あやか、よ', '*S2END\n「……あやか、よ')
+	#選択肢3
+	txt = txt.replace(r';JMP_CNT5_', 'mov $60,""')
+	txt = txt.replace(r';JMP_CNT6_', 'if %200==1 if %201==1 mov $60,"'+jmp_list[6]+'"')
+	txt = txt.replace(r';JMP_CNT7_', r'select "'+jmp_list[4]+r'",*S3A,"'+jmp_list[5]+r'",*S3B,$60,*S3C'+'\n*S3A\nmov %100,1')
+	txt = txt.replace(r'……どち', 'goto *S3END\n*S3B\nmov %100,2\n……どち')
+	txt = txt.replace(r'……ダメだ。', 'goto *S3END\n*S3C\nmov %100,3\n……ダメだ。')
+	txt = txt.replace('でいないと。\\', 'でいないと。\\\n*S3END\n')
+	txt = txt.replace(r'goto *SCR_A0230', r'if %100==1 goto *SCR_A0230')
+	txt = txt.replace(r'goto *SCR_B0450', r'if %100==2 goto *SCR_B0450')
+	txt = txt.replace(r'goto *SCR_C0690', r'if %100==3 goto *SCR_C0690')
+	#選択肢4
+	txt = txt.replace(r';JMP_CNT8_', r'select "'+jmp_list[7]+r'",*S4A,"'+jmp_list[8]+r'",*S4B'+'\n*S4A')
+	txt = txt.replace(r'正直、ち', '*S4B\n正直、ち')
+	#選択肢5
+	txt = txt.replace(r';JMP_CNT10_', r'select "'+jmp_list[9]+r'",*S5A,"'+jmp_list[10]+r'",*S5B'+'\n*S5A')
+	txt = txt.replace('深く息をついた。\\', '深く息をついた。\\\ngoto *S5END\n*S5B')
+	txt = txt.replace('体を擦り寄らせた。\\', '体を擦り寄らせた。\\\n*S5END')
+	txt = txt.replace(r';SCR_a0650z_END', 'bgmstop:dwavestop -1:csp -1:reset')
+	#選択肢6
+	txt = txt.replace(r';JMP_CNT12_', r'select "'+jmp_list[11]+r'",*S6A,"'+jmp_list[12]+r'",*S6B'+'\n*S6B')
+	txt = txt.replace(r'goto *SCR_A0420', 'goto *SCR_A0420\n*S6A')
+	txt = txt.replace(r';SCR_A0660_END', 'bgmstop:dwavestop -1:csp -1:reset')
+	txt = txt.replace(r';SCR_A0440Z_END', 'bgmstop:dwavestop -1:skipoff:mov %200,1:bgmonce "bgm\\Led_a.ogg":bg "image\\ed1_ayaka.png",10:click:bgmstop:dwavestop -1:csp -1:reset')#ED1
+	#選択肢7
+	txt = txt.replace(r';JMP_CNT14_', r'select "'+jmp_list[13]+r'",*S7A,"'+jmp_list[14]+r'",*S7B'+'\n*S7A')
+	txt = txt.replace(r'goto *SCR_B0540A', 'goto *SCR_B0540A\n*S7B')
+	txt = txt.replace(r';SCR_B0670_END', 'bgmstop:dwavestop -1:csp -1:reset')
+	#選択肢8
+	txt = txt.replace(r';JMP_CNT16_', r'select "'+jmp_list[15]+r'",*S8A,"'+jmp_list[16]+r'",*S8B'+'\n*S8B')
+	txt = txt.replace(r'goto *SCR_B0630', 'goto *SCR_B0630\n*S8A')
+	txt = txt.replace(r';SCR_b0680z_END', 'bgmstop:dwavestop -1:csp -1:reset')
+	txt = txt.replace(r';SCR_B0640Z_END', 'bgmstop:dwavestop -1:skipoff:mov %201,1:bgmonce "bgm\\Led_a.ogg":bg "image\\ed2_ayaka.png",10:click:bgmstop:dwavestop -1:csp -1:reset')#ED2
+	#選択肢9
+	txt = txt.replace(r';JMP_CNT18_', r'select "'+jmp_list[17]+r'",*S9A,"'+jmp_list[18]+r'",*S9B'+'\n*S9A\nmov %101,1')
+	txt = txt.replace('の準備を始めた。\\', 'の準備を始めた。\\\ntati_reset')
+	txt = txt.replace('るさを感じた。\\', 'るさを感じた。\\\ngoto *SCR_C0700Z\n*S9B\nmov %101,2')
+	txt = txt.replace('着てもらう。\\', '着てもらう。\\\nif %101==2 goto *S9SKIP')
+	txt = txt.replace('かは顔を見合わせ、それからにっこりと笑って言った。\\', 'かは顔を見合わせ、それからにっこりと笑って言った。\\\ngoto *S9END\n*S9SKIP')
+	txt = txt.replace('華は顔を見合わせ、それからにっこりと笑って言った。\\', '華は顔を見合わせ、それからにっこりと笑って言った。\\\n*S9END')
+	txt = txt.replace(';SCR_C0700Z_END', 'tati_reset:csp -1:bgmstop:dwavestop -1:skipoff:mov %202,1:bgmonce "bgm\\Led_s.ogg":bg "image\\ed3_harem.png",10:click:bgmstop:dwavestop -1:csp -1:reset')#ED3
+
+	open(os.path.join(same_hierarchy,'0.txt'), 'w', errors='ignore').write(txt)
+
+
+def file_check():
+	c = True
+	for p in [EXE_IGS, DEFAULT_TXT, DIR_SCR, DIR_BG, DIR_FG, DIR_SYS]:
+		if not os.path.exists(p):
+			print(p+ ' is not found!')
+			c = False
+	
+	return c
+
+
+def junk_del():
+	shutil.rmtree(DIR_BG)
+	shutil.rmtree(DIR_FG)
+	shutil.rmtree(DIR_SYS)
+	shutil.rmtree(DIR_SCR)
+	shutil.rmtree(DIR_SCR_DEC)
+	os.remove(DEFAULT_TXT)
+
+
+if file_check():
+	img_resize()
+	text_dec()
+	text_cnv()
+	junk_del()
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # IGS2ONS_Noesis-loveesm
- 'ラブesエム'ONSコンバータ
+## なにこれ
+  2013年にNoesis様から発売された、18禁PC向けノベルゲーム'[ラブesエム](http://www.gungnir.co.jp/noesis/products/lovesm/index.html)'を<br>
+  ONScripter形式へ変換するためのコンバータです<br>
+
+## 再現度
+原作との違いは主に以下
+ - 画面特殊効果全般の実装が不完全(というか無)
+ - 大多数の画像遷移は単純なフェードで代用
+ - wait処理の待ち時間が原作と違う
+ - セピア/モノクロ処理一切なし
+ - セーブ/ロード画面は超簡略化
+ - CG/音楽/回想モードは未実装<br>
+   (GALLERYはとりあえずOPを流せるようになってます)
+
+
+## 使い方
+ 1. 適当な作業フォルダを作成
+ 2. [GARBro](https://drive.google.com/file/d/1gH9nNRxaz8GexN0B1hWyUc3o692bkWXX/view)で全ての.igaを(同名のフォルダを作成した上で)作業フォルダへ展開<br>
+     設定は以下の通り↓<br>
+     ![](image1.png)
+ 3. [IG_tools](https://github.com/lennylxx/IG_tools/releases/tag/v1.0.0)をDLし展開、**"igscriptD.exe"のみ**を作業フォルダへ移動<br>
+ 4. 展開先のディレクトリで[このコンバータ](https://github.com/Prince-of-sea/IGS2ONS_Noesis-loveesm/releases/latest)をDL/起動させ変換(最低でも数分程度はかかります)<br>
+    変換前の時点で以下のような構成になっていればOKです↓<br>
+    ![](image2.png)<br>
+ 5. ウィンドウが消え、0.txtができれば完成<br>
+    exe等の不要データを削除し、変換済みファイルと共に利用ハードへ転送
+
+## 注意事項
+ - 当然ですが公式ツールではありません
+ - __FANZA DL版で動作確認しています__ パッケージ版の動作は未確認
+ - 本ツールの使用において生じた問題や不利益などについて、作者は一切の責任を負いません
+
+## その他
+ - 本作の変換を追加でサポートする[PSP向け自動変換ツール作ってます](https://github.com/Prince-of-sea/ONScripter_Multi_Converter)<br>
+   もしPSPで遊ぶ場合はぜひご利用ください(v1.2.9以上推奨)
+ - ゲームをプレイする際に利用する端末やONScripterのバージョンによっては、<br>
+   OPムービーの音が途切れたり、スロー再生になったりする場合があります<br>
+   適宜FFmpeg等で調整してください<br>
+   (PSPの場合は上記ツールを利用すれば自動で変換してくれるため特に作業は不要です)<br>
